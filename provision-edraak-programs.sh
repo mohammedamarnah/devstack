83368f247580bde8b9603d49c4cd910cabde8f63
Saad Altabari <stabari@qrf.org>
2020-12-15 16:54:08 +0200

    update volume paths

--- provision-edraak-programs.sh
+++ provision-edraak-programs.sh
@@ -11,12 +11,13 @@ echo "** Progs: Migrating databases **"
 docker-compose exec edraak_programs bash -c 'python manage.py migrate --settings=edraakprograms.dev'
 
 echo "** Progs: Compiling assets **"
-docker-compose exec edraak_programs bash -c 'npm rebuild node-sass'
-docker-compose exec edraak_programs bash -c 'chown -R root ~/.npm'
-docker-compose exec edraak_programs bash -c 'npm install -g'
-docker-compose exec edraak_programs bash -c 'npm install'
-docker-compose exec edraak_programs bash -c 'npm run dev'
-docker-compose exec edraak_programs bash -c 'python manage.py collectstatic --ignore="*.less" --ignore="*.scss" --noinput --clear --settings=edraakprograms.dev'
+#docker-compose exec edraak_programs bash -c 'npm rebuild node-sass'
+#docker-compose exec edraak_programs bash -c 'chown -R root ~/.npm'
+#docker-compose exec edraak_programs bash -c 'npm install -g'
+#docker-compose exec edraak_programs bash -c 'npm install'
+#docker-compose exec edraak_programs bash -c 'npm run dev'
+#docker-compose exec edraak_programs bash -c 'python manage.py compilestatic --settings=edraakprograms.static'
+#docker-compose exec edraak_programs bash -c 'python manage.py collectstatic --ignore="*.less" --ignore="*.scss" --noinput --clear --settings=edraakprograms.dev'
 
 echo "** Progs: Restarting **"
 docker-compose restart edraak_programs
